816d320e0af3a555489e2b8630aa0f160ffd059c
duytai800 <duytai800@gmail.com>
2021-11-26 19:48:46 +0700

    add the problem

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -1,3 +1,11 @@
+# Given an array of integers nums and an integer target, return indices of the 
+# two numbers such that they add up to target.
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
             for i in range(len(nums)):
