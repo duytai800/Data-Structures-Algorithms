263e5d0ded090986b955e3ea6ad3355a3a4806ae
duytai800 <duytai800@gmail.com>
2022-01-20 09:47:27 +0700

    update idea and solution

--- Leetcode/3. Longest Substring Without Repeating Characters.py
+++ Leetcode/3. Longest Substring Without Repeating Characters.py
@@ -8,6 +8,11 @@
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 # IDEA: 
+# TODO: We use "Slide window algorithm" in this problem
+# We create a set called "CheckSubstring" to determine substring, as well as calculate the lengest length of it.
+# We check single character in the string "s"
+# ? If that character exist in "Checksubstring": Delete the second character, and continue to next character
+# ? IF that character doesn't exist in "Checksubstring": add that character to "Checksubstring"
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
